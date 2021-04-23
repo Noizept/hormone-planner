1ea156c82b4c4a2fb2bec7a7738091286a6308b1
Sergio Viula <sergioviula@Sergios-MacBook-Pro.local>
2021-04-23 17:20:43 +0300

    prisma hormone service

--- user-service/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { initConnection } from "./database/connection";
-import startServer from "./server";
-
-initConnection().then(() => {
-  console.log("Database Connected");
-  startServer();
-});
